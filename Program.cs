73c0008e94366394818c29d20e49006914a20061
CesarChiodi <cesarchiodi@outlook.com>
2022-03-07 18:22:17 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sequencia_fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int parada, aux, numero = 0, numero2 = 1;
+
+            Console.WriteLine("escreva a parada");
+            parada = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("0\n1");
+
+            for (int i = 0; i < parada; i++)
+            {
+                aux = numero;
+                numero = numero2;
+                numero2 = numero + aux;
+                Console.WriteLine(numero2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+ 
